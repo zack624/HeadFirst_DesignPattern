c2e889c2f33894ca18bb9208d2dace5a1977810a
zackhome <zack@Luomu>
2016-08-18 00:17:19 +0800

    Virtual Proxy is completed.

--- src/edu/nust/structural/proxypattern/virtualproxy/IconProxy.java
+++ src/edu/nust/structural/proxypattern/virtualproxy/IconProxy.java
@@ -2,6 +2,7 @@ package edu.nust.structural.proxypattern.virtualproxy;
 
 import java.awt.Component;
 import java.awt.Graphics;
+import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -10,21 +11,53 @@ public class IconProxy implements Icon {
 	private ImageIcon imageIcon;
 	private int width;
 	private int height;
-	
+	//用于判定是否需要创建imageIcon实例
+	private boolean flag = false;
 	
 	@Override
-	public void paintIcon(Component c, Graphics g, int x, int y) {
-		
+	public void paintIcon(final Component c, Graphics g, int x, int y) {
+		if(imageIcon != null){
+			imageIcon.paintIcon(c, g, x, y);
+		}else{
+			g.drawString("iu is loading", x+100, y+50);
+			if(!flag){
+				flag = true;
+				//新建一个线程实例化真正的调用对象
+				Thread t = new Thread(new Runnable() {
+					@Override
+					public void run() {
+						try{
+							Thread.sleep(3000);
+							URL url = new URL("http://e.hiphotos.baidu.com/baike/c0%3Dbaike116%2C5%2C5%2C116%2C38/sign=6fea50488044ebf8797c6c6db"
+								+ "890bc4f/b3119313b07eca8061b250ac962397dda04483c1.jpg");
+							imageIcon = new ImageIcon(url,"iu");
+							c.repaint();
+						}catch(Exception e){
+							e.printStackTrace();
+						}
+					}
+				});
+				t.start();
+			}
+		}
 	}
 
 	@Override
 	public int getIconWidth() {
-		return width;
+		if(imageIcon != null){
+			return imageIcon.getIconWidth();
+		}else{
+			return 500;
+		}
 	}
 
 	@Override
 	public int getIconHeight() {
-		return height;
+		if(imageIcon != null){
+			return imageIcon.getIconHeight();
+		}else{
+			return 500;
+		}
 	}
 
 }
--- src/edu/nust/structural/proxypattern/virtualproxy/LoadingDemo.java
+++ src/edu/nust/structural/proxypattern/virtualproxy/LoadingDemo.java
@@ -1,5 +1,49 @@
 package edu.nust.structural.proxypattern.virtualproxy;
 
-public class LoadingDemo {
+import java.awt.Graphics;
 
+import javax.swing.Icon;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+public class LoadingDemo {
+	
+	public static void main(String[] args) {
+		new LoadingDemo().run();
+	}
+	public void run(){
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Icon icon = new IconProxy();
+//		ImageComponent ic = new ImageComponent(icon);
+//		frame.getContentPane().add(ic);
+		JButton button = new JButton(icon);
+		frame.getContentPane().add(button);
+		frame.setSize(500,500);
+		frame.setVisible(true);
+		
+	}
+	//专门装icon的容器
+	public class ImageComponent extends JComponent {  
+	    private Icon icon;  
+	  
+	    public ImageComponent(Icon icon) {  
+	        this.icon = icon;  
+	    }  
+	  
+	    public void setIcon(Icon icon) {  
+	        this.icon = icon;  
+	    }  
+	  
+	    public void paintComponent(Graphics g) {  
+	        super.paintComponent(g);  
+	        int w = icon.getIconWidth();  
+	        int h = icon.getIconHeight();  
+	        int x = (800 - w)/2;  
+	        int y = (600 - h)/2;  
+	        icon.paintIcon(this, g, x, y);  
+	    }  
+	}  
 }
